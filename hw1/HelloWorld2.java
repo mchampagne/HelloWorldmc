8b6fcbc9e2ff01af4fee952b828dbd5e22e51ab1
Maurice <mchampagne41@gmail.com>
2011-09-15 19:32:53 -0400

    foobar

---
+++ hw1/HelloWorld2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld2;
+
+/**
+ *
+ * @author Maurice
+ */
+public class HelloWorld2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello World");  // TODO code application logic here
+    }
+}
---
+++ hw1/HelloWorldmc.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldmc;
+
+/**
+ *
+ * @author Maurice
+ */
+public class HelloWorldmc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   System.out.print("Hello World");}
+           // TODO code application logic here
+    }
+}
